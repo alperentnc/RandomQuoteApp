8a9dfd5802d4747d200a02d14a7c43b9436170a4
Alperen Tunç <alprn_08_@hotmail.com>
2026-01-19 14:56:17 +0300

    Initial Commit

---
+++ RandomQuoteApp/RandomQuoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAppApp.swift
+//  RandomQuoteApp
+//
+//  Created by Alperen Tunç on 19.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomQuoteAppTests/RandomQuoteAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAppTests.swift
+//  RandomQuoteAppTests
+//
+//  Created by Alperen Tunç on 19.01.2026.
+//
+
+import Testing
+@testable import RandomQuoteApp
+
+struct RandomQuoteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
