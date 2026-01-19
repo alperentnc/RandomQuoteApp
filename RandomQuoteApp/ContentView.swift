27830dda711d0d8d4690354085397bb1777c3327
Alperen Tunç <alprn_08_@hotmail.com>
2026-01-19 15:37:09 +0300

    Add initial quote UI with random quote logic

--- RandomQuoteApp/ContentView.swift
+++ RandomQuoteApp/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentQuote : Quote = Quote.mockQuotes.randomElement()!
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 24){
+            Text("“\(currentQuote.text)”")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .padding()
+            Text("- \(currentQuote.author)")
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+            Button("New Quote"){
+                currentQuote = Quote.mockQuotes.randomElement()!
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
